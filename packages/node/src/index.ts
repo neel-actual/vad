8c2271941a6767e72576c32f301d73ff4fbb6be6
Neel <neel@rocketbots.io>
2024-10-14 12:57:54 +0800

    add realtime vad

--- packages/node/src/index.ts
+++ packages/node/src/index.ts
@@ -1,4 +1,5 @@
 import * as ort from "onnxruntime-node"
+import { modelFetcher } from "./model-fetcher"
 import {
   utils,
   PlatformAgnosticNonRealTimeVAD,
@@ -6,24 +7,16 @@ import {
   FrameProcessorOptions,
   Message,
   NonRealTimeVADOptions,
-  Resampler,
 } from "./_common"
-import * as fs from "fs/promises"
-
-const modelPath = `${__dirname}/silero_vad.onnx`
-
-const modelFetcher = async (): Promise<ArrayBuffer> => {
-  const contents = await fs.readFile(modelPath)
-  return contents.buffer
-}
 
 class NonRealTimeVAD extends PlatformAgnosticNonRealTimeVAD {
   static async new(
-    options: Partial<NonRealTimeVADOptions> = {}
+      options: Partial<NonRealTimeVADOptions> = {}
   ): Promise<NonRealTimeVAD> {
     return await this._new(modelFetcher, ort, options)
   }
 }
 
-export { utils, Resampler, FrameProcessor, Message, NonRealTimeVAD }
+export { utils, FrameProcessor, Message, NonRealTimeVAD }
 export type { FrameProcessorOptions, NonRealTimeVADOptions }
+export { RealTimeVAD } from "./real-time-vad"
